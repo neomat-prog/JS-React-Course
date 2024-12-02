735d0cdceb70cfbd5168607538aee5c5b7f602f4
neomat-prog <thegreyliongaming@gmail.com>
2024-12-02 21:22:51 +0100

    Fetch and Insert data into the backend server , Handle Note Changes from True to False and False to True

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import './index.css'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-
-createRoot(document.getElementById('root')).render(
+root.render(
   <StrictMode>
-    <App notes={notes}/>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
